0bde8548e6187b04449fcc692fd3db25d536fb1f
email <29516927+TheRealFame@users.noreply.github.com>
2026-08-24 13:41:29 -0400

    fix(ci): fix capacitor config webDir path and fix probe artifact upload path

--- tools/os-installers/capacitor.config.ts
+++ tools/os-installers/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'org.cutefame.nearcade',
   appName: 'Nearcade',
-  webDir: '../dist-android'
+  webDir: '../../dist-android'
 };
 
 export default config;
